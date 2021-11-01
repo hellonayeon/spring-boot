6dffc7d8ed508fa352b5ac3bd35634a0e9c54c79
hellonayeon <hellonayeon@icloud.com>
2021-11-01 16:50:03 +0900

    Servlet 웹 서비스 구현

--- select-shop/src/main/java/com/sparta/selectshop/SelectShopApplication.java
+++ select-shop/src/main/java/com/sparta/selectshop/SelectShopApplication.java
@@ -2,8 +2,12 @@ package com.sparta.selectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
+@ServletComponentScan
 public class SelectShopApplication {
 
     public static void main(String[] args) {

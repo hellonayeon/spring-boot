5a28c4e0fa2e562a501d2aa1ef0e983413df44c0
hellonayeon <hellonayeon@icloud.com>
2021-11-12 21:37:31 +0900

    상품 추가 및 업데이트 API 구현

---
+++ springcore/src/main/java/com/sparta/springcore/domain/Product.java
@@ -0,0 +1,41 @@
+package com.sparta.springcore.domain;
+
+import com.sparta.springcore.dto.ProductRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Product extends Timestamped {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String link;
+
+    @Column(nullable = false)
+    private int lprice;
+
+    @Column(nullable = false)
+    private int myprice;
+
+    public Product(ProductRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.image = requestDto.getImage();
+        this.link = requestDto.getLink();
+        this.lprice = requestDto.getLprice();
+        this.myprice = 0;
+    }
+}
---
+++ springcore/src/main/java/com/sparta/springcore/dto/ProductMypriceRequestDto.java
@@ -0,0 +1,8 @@
+package com.sparta.springcore.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}

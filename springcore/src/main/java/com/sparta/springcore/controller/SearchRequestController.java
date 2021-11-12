19030423ab405d21e00a760926bda7f8959a023f
hellonayeon <hellonayeon@icloud.com>
2021-11-12 16:45:10 +0900

    네이버 쇼핑 API 연동

---
+++ springcore/src/main/java/com/sparta/springcore/controller/SearchRequestController.java
@@ -0,0 +1,22 @@
+package com.sparta.springcore.controller;
+
+import com.sparta.springcore.dto.ItemDto;
+import com.sparta.springcore.util.NaverShopSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class SearchRequestController {
+    private final NaverShopSearch naverShopSearch;
+
+    @GetMapping("/api/search")
+    public List<ItemDto> getItems(@RequestParam String query) {
+        String resultString = naverShopSearch.search(query);
+        return naverShopSearch.fromJSONtoItems(resultString);
+    }
+}

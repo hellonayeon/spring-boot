9f110e858dcddcfe4fa6090df837342ae860420e
hellonayeon <hellonayeon@icloud.com>
2021-11-15 10:41:51 +0900

    도커 컨테이너 설정

--- demo/src/main/java/com/sparta/demo/domain/LectureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.demo.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// 인터페이스: 기능만 정의, 실제 구현은 JpaRepository 클래스
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
-}
--- demo/src/test/java/com/docker/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/docker/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sparta.demo;
+package com.docker.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
